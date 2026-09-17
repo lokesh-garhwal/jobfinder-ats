ec3b31fa2cd6a96a47a0e62b1661e6f3473f097c
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2026-09-17 23:09:23 +0530

    Removed hardcoded localhost and added global Axios URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js' // Required for Dropdowns to open
 import './App.css'
 
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
